7f641f511a32ff45f1791b450872144b59211178
Batura Andrey <batura.andrey@otr.ru>
2022-10-20 17:02:38 +0300

    fix: clearing

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function getCol(pos) {
 }
 
 function startGame() {
+  cubes = [];
   let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, " "];
 
   values = values.sort(() => Math.random() - 0.5);
